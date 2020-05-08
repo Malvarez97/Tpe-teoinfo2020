2a0ccf1f68a4f2357b88a8251a178b00487e5ccb
Malvarez97 <maurialva.ma@gmail.com>
2020-05-08 13:10:58 -0300

    Imagen ingresada a matriz y testeada

---
+++ src/teo/info/Imagen.java
@@ -0,0 +1,52 @@
+package teo.info;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+
+public class Imagen {
+    public Imagen() {
+    }
+
+    private BufferedImage getImage(String filename) {
+        try {
+            InputStream in = getClass().getResourceAsStream(filename);// ingresa el directorio en la entrada que luego es cargada en la
+            return ImageIO.read(in);
+        } catch (IOException e) {
+            System.out.println("The image was not loaded.");
+        }
+        return null;
+    }
+
+    public int[][] getValoresMat(String nombrefoto) {// ¿podriamos agregarle que ponga el nombre de la foto a ingresar y lo haga automatico del Main ?
+        // creo la imagen
+        BufferedImage imagen = this.getImage("/"+nombrefoto+".jpg");//lee lo que hay en la carpeta imagenes ..curiosidad la lee de la carpeta la primera vez y hacer una copia de "segurirdad en la carpeta out de produccion, si la borras , queda guardad en esa copia y se puede volver a usar .
+        /** recorrer el buffer e ingreso los datos en la matriz */
+        int matriz[][] = new int[imagen.getWidth()][imagen.getHeight()];
+        for (int alto = 0; alto < imagen.getHeight(); alto++) {
+            for (int ancho = 0; ancho < imagen.getWidth(); ancho++) {
+                matriz[ancho][alto] = imagen.getRGB(ancho, alto);
+            }
+        }
+
+        /**imprimo la matriz (para testear)
+        for (int a = 0; a < imagen.getWidth(); a++) {
+            System.out.println();
+            for (int l = 0; l < imagen.getHeight(); l++) {
+                System.out.print(matriz[a][l]);
+            }
+        }
+        System.out.println();
+        System.out.println("largo de imagen  " + imagen.getHeight());
+        System.out.println("ancho de la imagen  " + imagen.getWidth());
+         */ //
+        return matriz;
+    }
+}
+
+
+
+
+
+
---
+++ src/teo/info/Main.java
@@ -0,0 +1,9 @@
+package teo.info;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 Imagen A=new Imagen();
+	 A.getValoresMat("mundo");//si hacemos interfaz hacer que el usuario meta el nombre de la foto
+    }
+}
