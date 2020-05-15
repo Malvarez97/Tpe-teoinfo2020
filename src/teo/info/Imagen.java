c2d27cc57cb5d7d5e4c08d01ace27720799bb51b
Malvarez97 <maurialva.ma@gmail.com>
2020-05-14 21:03:23 -0300

    lectura arreglada

--- src/teo/info/Imagen.java
+++ src/teo/info/Imagen.java
@@ -1,5 +1,7 @@
 package teo.info;
 
+
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,12 +23,13 @@ public class Imagen {
 
     public int[][] getValoresMat(String nombrefoto) {// ¿podriamos agregarle que ponga el nombre de la foto a ingresar y lo haga automatico del Main ?
         // creo la imagen
-        BufferedImage imagen = this.getImage("/"+nombrefoto+".jpg");//lee lo que hay en la carpeta imagenes ..curiosidad la lee de la carpeta la primera vez y hacer una copia de "segurirdad en la carpeta out de produccion, si la borras , queda guardad en esa copia y se puede volver a usar .
+        BufferedImage imagen = this.getImage("/"+nombrefoto+".bmp");//lee lo que hay en la carpeta imagenes ..curiosidad la lee de la carpeta la primera vez y hacer una copia de "segurirdad en la carpeta out de produccion, si la borras , queda guardad en esa copia y se puede volver a usar .
         /** recorrer el buffer e ingreso los datos en la matriz */
         int matriz[][] = new int[imagen.getWidth()][imagen.getHeight()];
         for (int alto = 0; alto < imagen.getHeight(); alto++) {
             for (int ancho = 0; ancho < imagen.getWidth(); ancho++) {
-                matriz[ancho][alto] = imagen.getRGB(ancho, alto);
+                Color Tpixel =new Color(imagen.getRGB(ancho, alto),true); /** devuelve el tono de color del pixel(en este caso el azul , pero podria ser por cualquiera de los 3 por que los tonos de gris tienen la misma cantidad de cada color*/
+                matriz[ancho][alto] =Tpixel.getBlue() ;
             }
         }
 
@@ -40,7 +43,7 @@ public class Imagen {
         System.out.println();
         System.out.println("largo de imagen  " + imagen.getHeight());
         System.out.println("ancho de la imagen  " + imagen.getWidth());
-         */ //
+        */
         return matriz;
     }
 }
--- src/teo/info/Main.java
+++ src/teo/info/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	 Imagen A=new Imagen();
-	 A.getValoresMat("mundo");//si hacemos interfaz hacer que el usuario meta el nombre de la foto
+	 A.getValoresMat("Will(Original)");//si hacemos interfaz hacer que el usuario meta el nombre de la foto
     }
 }
